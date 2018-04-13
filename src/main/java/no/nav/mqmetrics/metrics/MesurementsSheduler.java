5b6180d6846439004df3608c5291874de828eda9
Esben Stenwig <esben.stenwig@nav.no>
2018-04-13 13:18:37 +0200

    MFN-772: Bestilte egen kanal

--- src/main/java/no/nav/mqmetrics/metrics/MesurementsSheduler.java
+++ src/main/java/no/nav/mqmetrics/metrics/MesurementsSheduler.java
@@ -2,12 +2,18 @@ package no.nav.mqmetrics.metrics;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.annotation.After;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
+import static java.time.Duration.ofMillis;
+
 @Component
-public class MesurementsSheduler {
+public class MesurementsSheduler implements InitializingBean {
 
     @Autowired
     private MeasurementsService measurementsService;
@@ -15,10 +21,23 @@ public class MesurementsSheduler {
     @Autowired
     private MeterRegistry registry;
 
+    private Timer schedulerTimer;
+
     @Scheduled(fixedRateString = "${MQMETRICS_SCHEDULER_DELAY:15000}")
     public void doUpdate() {
         Timer.Sample sample = Timer.start(registry);
         measurementsService.updateMeasurements();
-        sample.stop(registry.timer("freg.mq.metrics.timed", "operation", "updateMesurementsSheduler"));
+        sample.stop(schedulerTimer);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        schedulerTimer  = Timer.builder("freg.mq.metrics.timed")
+                .tag("operation", "updateMesurementsSheduler")
+                .publishPercentiles(.25,.50,.75,.90)
+                .maximumExpectedValue(Duration.ofSeconds(5))
+                .sla(ofMillis(1000),ofMillis(1500),ofMillis(2000),ofMillis(2500),ofMillis(3000))
+                .register(registry);
+    }
+
 }
