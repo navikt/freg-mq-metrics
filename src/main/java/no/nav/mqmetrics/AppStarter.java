6f3c58a13fb53721c3a7c465040500ff60750152
Tsigab Angosom Gebremedhin <tsigab.angosom.gebremedhin@nav.no>
2021-05-18 15:51:42 +0200

    MMA-5306 inkludert i config  properties

--- src/main/java/no/nav/mqmetrics/AppStarter.java
+++ src/main/java/no/nav/mqmetrics/AppStarter.java
@@ -1,5 +1,6 @@
 package no.nav.mqmetrics;
 
+import no.nav.mqmetrics.config.MqAdminProperties;
 import no.nav.mqmetrics.metrics.MqProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableConfigurationProperties({MqProperties.class})
+@EnableConfigurationProperties({MqProperties.class, MqAdminProperties.class})
 public class AppStarter {
 
     public static void main(String[] args) {
