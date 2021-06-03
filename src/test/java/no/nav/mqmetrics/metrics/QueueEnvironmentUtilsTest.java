7b5f5cbb5b5811a4d8309dd56b5f750258403ffa
Tsigab Gebremedhin <tsigab.angosom@gmail.com>
2021-06-03 12:02:58 +0200

    MMA-5305 fjernet junit dependency

--- src/test/java/no/nav/mqmetrics/metrics/QueueEnvironmentUtilsTest.java
+++ src/test/java/no/nav/mqmetrics/metrics/QueueEnvironmentUtilsTest.java
@@ -1,43 +1,36 @@
 package no.nav.mqmetrics.metrics;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
 
-@RunWith(Parameterized.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
 public class QueueEnvironmentUtilsTest {
 
 
-    private String input, expexted;
-
-    public QueueEnvironmentUtilsTest(String input, String expexted) {
-        this.input = input;
-        this.expexted = expexted;
-    }
-
-    @Parameterized.Parameters(name = "{index}: queue[{0}] = env= {1}")
-    public static Iterable<Object[]> data() {
-        return Arrays.asList(
-                testCase("QA.I11_HEND.SAKSBEHANDLING_BQ", "I11"),
-                testCase("QA.U1_SBEH.SAKSBEHANDLING", "U1"),
-                testCase("QA.P_SBEH.SAKSBEHANDLING", "P"),
-                testCase("QA.SBEH.SAKSBEHANDLING", "UNKNOWN"),
-                testCase("QA.P460.BREV_REPLY_QUE", "P"),
-                testCase("QA.PKKC.CLUSTER.TEST.MPLSC02", "P"),
-                testCase("QA.U5_STAT.STATISTIKK_BQ", "U5")
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of("QA.I11_HEND.SAKSBEHANDLING_BQ", "I11"),
+                Arguments.of("QA.U1_SBEH.SAKSBEHANDLING", "U1"),
+                Arguments.of("QA.P_SBEH.SAKSBEHANDLING", "P"),
+                Arguments.of("QA.SBEH.SAKSBEHANDLING", "UNKNOWN"),
+                Arguments.of("QA.P460.BREV_REPLY_QUE", "P"),
+                Arguments.of("QA.PKKC.CLUSTER.TEST.MPLSC02", "P"),
+                Arguments.of("QA.U5_STAT.STATISTIKK_BQ", "U5")
         );
     }
 
-    private static Object[] testCase(String input, String expected) {
-        return new Object[]{input, expected};
-    }
 
-    @Test
-    public void testName() {
-        assertEquals(QueueEnvironmentUtils.stripEnvironmentNameFrom(input), expexted);
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testName(String input, String expected) {
+        assertEquals(QueueEnvironmentUtils.stripEnvironmentNameFrom(input), expected);
     }
 }
